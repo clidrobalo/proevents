2b366939bddb9fa8879f94ad39abe8316883b51a
Clid Robalo <csrobalo@criticalsoftware.com>
2022-10-01 18:15:35 +0100

    FRONT - add form validation for registration

--- Front/ProEvents-App/src/app/components/user/registration/registration.component.ts
+++ Front/ProEvents-App/src/app/components/user/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+  registationForm!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  get form(): any {
+    return this.registationForm.controls;
   }
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.validation();
+  }
+
+  private validation(): void {
+    this.registationForm = this.fb.group({
+      fistName: ['', [Validators.required, Validators.minLength(4)]],
+      lastName: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      passwordConfirmed: ['', Validators.required]
+    });
+  }
+
+  public resetForm(): void {
+    this.registationForm.reset();
+  }
 }
