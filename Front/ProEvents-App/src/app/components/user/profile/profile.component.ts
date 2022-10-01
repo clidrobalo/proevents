750cff2366b673abad7f12d85d08624c2f34d11a
Clid Robalo <csrobalo@criticalsoftware.com>
2022-10-01 18:50:19 +0100

    FRONT - add validation to profile form

--- Front/ProEvents-App/src/app/components/user/profile/profile.component.ts
+++ Front/ProEvents-App/src/app/components/user/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/Validators';
 
 @Component({
   selector: 'app-profile',
@@ -7,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
   public title = "Profile";
+  profileForm!: FormGroup;
 
-  constructor() { }
+  get form(): any {
+    return this.profileForm.controls;
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.validation();
+  }
+
+  private validation(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('password', 'passwordConfirmed')
+    };
+
+    this.profileForm = this.fb.group({
+      title: ['', [Validators.required]],
+      fistName: ['', [Validators.required, Validators.minLength(4)]],
+      lastName: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required]],
+      role: ['', [Validators.required]],
+      description: [''],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      passwordConfirmed: ['', Validators.required]
+    }, formOptions);
+  }
+
+  public resetForm(): void {
+    this.profileForm.reset();
   }
 
 }
