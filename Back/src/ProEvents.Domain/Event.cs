3af2ea756ab7b3be636279a918213953fe720e68
Clid Robalo <csrobalo@criticalsoftware.com>
2022-10-02 10:08:42 +0100

    BACK - add data annotation in event domain

--- Back/src/ProEvents.Domain/Event.cs
+++ Back/src/ProEvents.Domain/Event.cs
@@ -1,17 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProEvents.Domain
 {
     public class Event
     {
         public int Id { get; set; }
+        [Required]
         public string Place { get; set; }
+        [Required]
         public DateTime? EventDate { get; set; }
+        [Required]
         public string Theme { get; set; }
+        [Required]
         public int NumberOfPerson { get; set; }
         public string ImageUrl { get; set; }
+        [Required]
         public string phone { get; set; }
+        [Required]
         public string Email { get; set; }
         public IEnumerable<Lote> Lotes { get; set; }
         public IEnumerable<SocialMedia> SocialMedias { get; set; }
